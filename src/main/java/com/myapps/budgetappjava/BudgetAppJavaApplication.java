bd834bf0e7afd9653b19515e171f0293258989fd
kevin-k-1984 <78624076+kevin-k-1984@users.noreply.github.com>
2021-11-11 23:35:57 -0600

    first commit

---
+++ src/main/java/com/myapps/budgetappjava/BudgetAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.myapps.budgetappjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetAppJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapps/budgetappjava/BudgetAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapps.budgetappjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetAppJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
